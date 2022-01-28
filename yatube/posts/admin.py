73575f836b4aefb9c9c2471715c071f53a69e28f
Дарья Мурова <murovadarya@gmail.com>
2022-01-29 00:32:32 +0300

    Пройдены все тесты

---
+++ yatube/posts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from django.db import models
+
+from .models import Group, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    # Добавим в начало столбец pk
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    # Это свойство сработает для всех колонок: где пусто — там будет эта строка 
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    search_fields = ('title',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
